062e7d59a2c6182f7e1c4ef5dec703da20e33600
Leyla Aziz <azizleyla01@gmail.com>
2021-05-03 19:24:52 +0400

    new steps for calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+const calcualtorDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
+function sendNumberValue(number) {
+    const displayValue = calcualtorDisplay.textContent;
+    calcualtorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+}
+inputBtns.forEach((inputBtn) => {
+    if (inputBtn.classList.length === 0) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    } else if (inputBtn.classList.contains('operator')) {
+        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    } else if (inputBtn.classList.contains('decimal')) {
+        inputBtn.addEventListener('click', () => sendNumberValue());
+    }
+});
